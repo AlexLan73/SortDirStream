ff3d744ec46dcc90a06c55197192394c7b585097
alexandr.lanin <alexlannami@gmail.com>
2020-07-21 09:17:25 +0300

    #8 мелочь

--- SortDirStream.py
+++ SortDirStream.py
@@ -11,13 +11,12 @@ logger, path_file_logger, is_prog, path_dan = {}, "", True, ""
 
 def direct(path_dir):
     global logger
-    if not(os.path.isdir(path_dir)):
-        logger.info(" создает dir - {}".format(path_dir))
+    if not (os.path.isdir(path_dir)):
         os.mkdir(path_dir)
         if os.path.isdir(path_dir):
-            logger.info(" dir - {}   # создана".format(path_dir))
+            logger.info(" dir -> {}".format(path_dir))
         else:
-            logger.warning(" dir - {}   # проблема создания ".format(path_dir))
+            logger.warning(" dir -> {}".format(path_dir))
 
 
 def read_dir_files():
@@ -59,7 +58,7 @@ def read_dir_files():
             path_dir = path_dir + "\\" + p[1]
             direct(path_dir)
             __path = path_dir + "\\" + _name_file_basa
-            logger.info(" - перемещаем файл {}".format(__path))
+#            logger.info(" - перемещаем файл {}".format(__path))
             if os.path.isfile(__path):
                 os.remove(__path)
             try:
@@ -104,34 +103,50 @@ class MyThread(threading.Thread):
 
 
 def parse_input_arguments():
-    global logger
-    name_file_run = __file__    # путь старта программы
+    name_file_run = __file__  # путь старта программы
+    _dargs = dict(
+        s_error="Ok!",
+        kod_error=0,
+        dir_start="",
+        dir_work="",
+        ls_arg=[]
+    )
+
     ls_arg = sys.argv
     k = 0
+
     for it in ls_arg:
         _s = " Номер аргумента -{} значение ->  {} ".format(k, it)
         print(_s)
-        logger.info(_s)
+        _dargs["ls_arg"] += [_s]
         k += 1
 
-    if len(ls_arg) < 1:
-        print(" нет файла в аргументах")
-        logger.critical(" нет файла в аргументах")
-        sys.exit(-1)
+    if len(ls_arg) < 2:
+        _dargs["kod_error"] = -1
+        _dargs["s_error"] = " нет  аргументов  код -1"
+        print(_dargs["s_error"])
+        return _dargs
 
-    if os.path.isdir(ls_arg[1]):
-        logger.info(" - обработка dir {}".format(ls_arg[1]))
-        return [ls_arg[0], ls_arg[1]]
+    _dargs["dir_start"] = os.path.dirname(ls_arg[0])
+    if not (os.path.isdir(_dargs["dir_start"])):
+        _dargs["kod_error"] = -2
+        _dargs["s_error"] = "Не правильно определился каталог старта програмы  код -2"
+        return _dargs
 
-    print("Проблема с аргументами {} - нет каталога ".format(ls_arg[1]))
-    logger.critical("Проблема с аргументами {} - нет каталога   <- !!! ".format(ls_arg[1]))
-    sys.exit(-1)
+    _dargs["dir_work"] = ls_arg[1]
+    if not (os.path.isdir(_dargs["dir_work"])):
+        _dargs["kod_error"] = -3
+        _dargs["s_error"] = "Нет директории с данными код -3"
+        return _dargs
+
+    return _dargs
 
 
 def inicial_logging():
     global logger, path_file_logger
-    _path_log = "C:\\CSM UniCAN2\\UNICAN CONVERTED FILES\\LOG"
-    if not(os.path.isdir(_path_log)):
+    _path_log = path_file_logger + "\\LOG"
+
+    if not (os.path.isdir(_path_log)):
         os.mkdir(_path_log)
 
     dictLogConfig = logging_dict(_path_log)
@@ -141,13 +156,50 @@ def inicial_logging():
     logger = logging.getLogger("exampleApp")
     logger.info("START")
 
+def fprint(x):
+    for it in x: print(it)
+
+def error_args(_args):
+    global logger, path_file_logger
+    if _args["kod_error"] != 0:
+        # проблема с конфигурацией
+        if _args["kod_error"] ==-1:
+            print(_args["s_error"] )
+            fprint(_args["ls_arg"])
+            sys.exit(_args["kod_error"])
+
+        elif _args["kod_error"] ==-2:
+            print(_args["s_error"] )
+            fprint(_args["ls_arg"])
+            sys.exit(_args["kod_error"])
+
+        elif _args["kod_error"] ==-3:
+            print(_args["s_error"] )
+            fprint(_args["ls_arg"])
+            path_file_logger = _args["dir_start"]
+            inicial_logging()
+            for it in _args["ls_arg"]:
+                logger.info(it)
+            logger.critical(_args["s_error"])
+            sys.exit(_args["kod_error"])
+
 
 if __name__ == "__main__":
     print(" == START ==")
-    inicial_logging()
+    __arguments = parse_input_arguments()
 
-    __path_start = parse_input_arguments()
-    path_dan = __path_start[1] + "\\*.*"
+    if __arguments["kod_error"] != 0:
+        # проблема с аргументами
+        error_args(__arguments)
+
+    print(__arguments["s_error"] )
+    fprint(__arguments["ls_arg"])
+    path_file_logger = __arguments["dir_start"]
+    inicial_logging()
+    for it in __arguments["ls_arg"]:
+        logger.info(it)
+
+    path_dan = __arguments["dir_work"] + "\\*.*"
 
     stopFlag = threading.Event()
     thread = MyThread(stopFlag, Start_module)
